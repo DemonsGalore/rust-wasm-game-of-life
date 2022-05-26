c74c6605453bee86446684aaee4a6efe9dbc79b9
Andreas Stopat <dev@demonsgalore.de>
2022-05-26 12:23:25 +0200

    feat: implement game of life

--- game-of-life/src/lib.rs
+++ game-of-life/src/lib.rs
@@ -1,10 +1,12 @@
 extern crate wasm_bindgen;
+extern crate js_sys;
 
 use wasm_bindgen::prelude::*;
+// use std::fmt;
 
 // Import the `window.alert` function from the Web.
 #[wasm_bindgen]
-extern {
+extern "C" {
     fn alert(s: &str);
 }
 
@@ -13,3 +15,147 @@ extern {
 pub fn greet(name: &str) {
     alert(&format!("Hello, {}", name));
 }
+
+#[wasm_bindgen]
+#[repr(u8)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub enum Cell {
+    Dead = 0,
+    Alive = 1,
+}
+
+#[wasm_bindgen]
+pub struct Universe {
+    width: u32,
+    height: u32,
+    cells: Vec<Cell>,
+}
+
+impl Universe {
+    fn get_index(&self, row: u32, column: u32) -> usize {
+        (row * self.width + column) as usize
+    }
+
+    fn live_neighbor_count(&self, row: u32, column: u32) -> u8 {
+        let mut count = 0;
+
+        for delta_row in [self.height - 1, 0, 1].iter().cloned() {
+            for delta_col in [self.width - 1, 0, 1].iter().cloned() {
+                if delta_row == 0 && delta_col == 0 {
+                    continue;
+                }
+
+                let neighbor_row = (row + delta_row) % self.height;
+                let neighbor_col = (column + delta_col) % self.width;
+                let index = self.get_index(neighbor_row, neighbor_col);
+                count += self.cells[index] as u8;
+            }
+        }
+        count
+    }
+}
+
+#[wasm_bindgen]
+impl Universe {
+    pub fn tick(&mut self) {
+        let mut next = self.cells.clone();
+
+        for row in 0..self.height {
+            for col in 0..self.width {
+                let index = self.get_index(row, col);
+                let cell = self.cells[index];
+                let live_neighbors = self.live_neighbor_count(row, col);
+
+                let next_cell = match (cell, live_neighbors) {
+                    // Rule 1: Any live cell with fewer than two live neighbours
+                    // dies, as if caused by underpopulation.
+                    (Cell::Alive, x) if x < 2 => Cell::Dead,
+                    // Rule 2: Any live cell with two or three live neighbours
+                    // lives on to the next generation.
+                    (Cell::Alive, 2) | (Cell::Alive, 3) => Cell::Alive,
+                    // Rule 3: Any live cell with more than three live
+                    // neighbours dies, as if by overpopulation.
+                    (Cell::Alive, x) if x > 3 => Cell::Dead,
+                    // Rule 4: Any dead cell with exactly three live neighbours
+                    // becomes a live cell, as if by reproduction.
+                    (Cell::Dead, 3) => Cell::Alive,
+                    // All other cells remain in the same state.
+                    (otherwise, _) => otherwise,
+                };
+
+                next[index] = next_cell;
+            }
+        }
+
+        self.cells = next;
+    }
+
+    pub fn new() -> Universe {
+        let width = 64;
+        let height = 64;
+
+        let cells = (0..width * height)
+            .map(|i| {
+                if i % 2 == 0 || i % 7 == 0 {
+                    Cell::Alive
+                } else {
+                    Cell:: Dead
+                }
+            })
+            .collect();
+
+        Universe { width, height, cells }
+    }
+
+    pub fn random() -> Universe {
+        let width = 64;
+        let height = 64;
+
+        let cells = (0..width * height)
+            .map(|_| {
+                if js_sys::Math::random() < 0.5 {
+                    Cell::Alive
+                } else {
+                    Cell:: Dead
+                }
+            })
+            .collect();
+
+        Universe { width, height, cells }
+    }
+
+    pub fn width(&self) -> u32 {
+        self.width
+    }
+
+    pub fn height(&self) -> u32 {
+        self.height
+    }
+
+    pub fn cells(&self) -> *const Cell {
+        self.cells.as_ptr()
+    }
+
+    /*
+    pub fn render(&self) -> String {
+        self.to_string()
+    }
+    */
+}
+
+/*
+impl fmt::Display for Universe {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        for line in self.cells.as_slice().chunks(self.width as usize) {
+            for &cell in line {
+                let symbol = if cell == Cell::Dead { '◻' } else { '◼' };
+
+                write!(f, "{}", symbol)?;
+            }
+            write!(f, "\n")?;
+        }
+
+        Ok(())
+    }
+}
+*/
--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,109 @@
-import init, { greet } from '../game-of-life/pkg/game_of_life';
+import init, { Cell, Universe } from '../game-of-life/pkg/game_of_life';
 import './style.scss';
 
-const app = document.querySelector<HTMLDivElement>('#app')!;
-app.innerHTML = `
-<h1>Hello Vite!</h1>
-    <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`;
+const CELL_SIZE = 10;
+const GRID_COLOR = '#DDDDDD';
+const ALIVE_COLOR = 'rgba(0, 0, 0, 0.5)';
+const DEAD_COLOR = '#FFFFFF';
 
-const button = document.querySelector<HTMLButtonElement>('#button')!;
+const SPEED = 8;
+let lastRenderTime = 0;
+let fps = 0;
 
-init().then(() => {
-    console.log('init wasm-pack');
-    greet('from vite!');
+const canvas = document.querySelector<HTMLCanvasElement>('#game-of-life-canvas')!;
+const fpsCounter = document.querySelector<HTMLTitleElement>('#fps')!;
+let ctx: CanvasRenderingContext2D | null = null;
 
+let universe: Universe;
+let width: number;
+let height: number;
 
-    button.addEventListener('click', () => {
-        console.log('click');
-        greet('Andreas');
-    });
+let memory: WebAssembly.Memory;
+
+// Wasm functionality is only available after initialization
+init().then((init) => {
+    memory = init.memory;
+
+    universe = Universe.random();
+    width = universe.width();
+    height = universe.height();
+
+    canvas.height = (CELL_SIZE + 1) * height + 1;
+    canvas.width = (CELL_SIZE + 1) * width + 1;
+    ctx = canvas.getContext('2d');
+
+    drawGrid();
+    drawCells();
+    requestAnimationFrame(renderLoop);
 });
+
+const renderLoop = (currentTime: number): void => {
+    requestAnimationFrame(renderLoop);
+
+    const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000;
+
+    if (secondsSinceLastRender < 1 / SPEED) return;
+
+    fps = 1 / secondsSinceLastRender;
+    fpsCounter.innerText = `FPS: ${fps.toFixed(2)}`;
+
+    lastRenderTime = currentTime;
+
+    universe.tick();
+
+    drawGrid();
+    drawCells();
+};
+
+const drawGrid = (): void => {
+    if (ctx === null) return;
+
+    ctx.beginPath();
+    ctx.strokeStyle = GRID_COLOR;
+
+    // Vertical lines
+    for (let i = 0; i <= width; i++) {
+        ctx.moveTo(i * (CELL_SIZE + 1) + 1, 0);
+        ctx.lineTo(i * (CELL_SIZE + 1) + 1, (CELL_SIZE + 1) * height + 1);
+    }
+
+    // Horizontal lines
+    for (let j = 0; j <= height; j++) {
+        ctx.moveTo(0, j * (CELL_SIZE + 1) + 1);
+        ctx.lineTo((CELL_SIZE + 1) * width + 1, j * (CELL_SIZE + 1) + 1);
+    }
+
+    ctx.stroke();
+};
+
+const getIndex = (row: number, column: number): number => {
+    return row * width + column;
+};
+
+const drawCells = (): void => {
+    if (ctx === null) return;
+
+    const cellsPtr = universe.cells();
+    const cells = new Uint8Array(memory.buffer, cellsPtr, width * height);
+
+    ctx.beginPath();
+
+    for (let row = 0; row < height; row++) {
+        for (let col = 0; col < width; col++) {
+            const index = getIndex(row, col);
+
+            ctx.fillStyle = cells[index] === Cell.Dead
+                ? DEAD_COLOR
+                : ALIVE_COLOR;
+
+            ctx.fillRect(
+                col * (CELL_SIZE + 1) + 1,
+                row * (CELL_SIZE + 1) + 1,
+                CELL_SIZE,
+                CELL_SIZE
+            );
+        }
+    }
+
+    ctx.stroke();
+};
